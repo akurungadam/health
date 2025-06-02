ba0f50b28f819f9d18dfcf0019c24b6e728626c0
Anoop Kurungadam <anoop@earthianslive.com>
2025-06-02 20:57:19 +0530

    feat: doctypes for FHIR interoperability, new module def

---
+++ healthcare/interoperability/doctype/fhir_profile/fhir_profile.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2025, earthians Health Informatics Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+# import frappe
+# from frappe import _
+from frappe.model.document import Document
+
+
+class FHIRProfile(Document):
+	pass
