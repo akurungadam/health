e7b40ea6a3be5815c783c46cc2c5f467d8dc44d5
Anoop Kurungadam <anoop@earthianslive.com>
2025-06-03 16:58:12 +0530

    feat: create fhir structure definitions from fhir version and fhir profile

--- healthcare/interoperability/doctype/fhir_structure_definition/fhir_structure_definition.py
+++ healthcare/interoperability/doctype/fhir_structure_definition/fhir_structure_definition.py
@@ -1,10 +1,17 @@
 # Copyright (c) 2025, earthians Health Informatics Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-from frappe.model.document import Document
-
 # import frappe
+from frappe.model.document import Document
 
 
 class FHIRStructureDefinition(Document):
-	pass
+	def autoname(self):
+
+		if not self.name:
+			self.name = f"{self.fhir_sd}-{self.sd_version}"
+
+			if self.fhir_profile:
+				self.name = f"{self.name}-{self.fhir_profile}-{self.fhir_version}"
+			else:
+				self.name = f"{self.name}-{self.fhir_version}"
--- healthcare/interoperability/doctype/fhir_version/fhir_version.js
+++ healthcare/interoperability/doctype/fhir_version/fhir_version.js
@@ -3,6 +3,26 @@
 
 frappe.ui.form.on("FHIR Version", {
 	refresh: function (frm) {
+		frm.add_custom_button(__("Load Structure Definitions"), () => {
 
+			frappe.confirm(__("This will import all Structure Definitions once again if done earlier, confirm?"), ()=> {
+
+				frm.call("import_structure_definitions")
+				.then(r => {
+					frappe.show_alert({
+						"indicator": "green",
+						"message": __("Request Queued"),
+					});
+				});
+
+			});
+
+		});
+	},
+
+	schema_file: function (frm) {
+		if (!frm.doc.package_file.endsWith(".tar.gz") && !frm.doc.package_file.endsWith(".tgz")) {
+			frappe.msgprint(__("Please upload a valid FHIR Package (.tgz / tar.gz) file."));
+		}
 	}
 });
