f30de023b33e55b92d9d2afed8f5d88eee05e7d0
Anoop Kurungadam <anoop@earthianslive.com>
2025-06-05 12:26:15 +0530

    feat: basic Resource Map implementation to map FHIR Resource / Elements with Frappe DocType / DocFields

--- healthcare/interoperability/doctype/fhir_resource_map/fhir_resource_map.py
+++ healthcare/interoperability/doctype/fhir_resource_map/fhir_resource_map.py
@@ -9,7 +9,7 @@ class FHIRResourceMap(Document):
 	def autoname(self):
 
 		if not self.name:
-			self.name = f"MAP-{self.frappe_doctype}-{self.fhir_resource}"
+			self.name = f"MAP-{self.frappe_doctype}-{self.fhir_structure_def}"
 
 			# append fhir profile and name
 			if self.fhir_profile:
@@ -20,12 +20,18 @@ class FHIRResourceMap(Document):
 	def validate(self):
 
 		missing = [
-			fm.fhir_path
-			for fm in self.field_map
-			if fm.min_card > 0 and not fm.frappe_field and not fm.default_value
+			fm.fhir_path for fm in self.map if fm.min > 0 and not fm.frappe_field and not fm.default_value
 		]
 		if missing:
 			frappe.throw(
-				"You must map or supply a default for these FHIR elements which are mandatory:\n  "
+				"You must map or supply a default value for these FHIR elements which are required as per Resource Structure Definition:\n  "
 				+ "\n  ".join(missing)
 			)
+
+	@frappe.whitelist()
+	def save_mapped_elements(self, elements):
+
+		for el in elements:
+			self.append("map", frappe._dict(el))
+
+		self.save()
