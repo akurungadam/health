e6a33f124e19edad42f9562362cea54895d5ff3b
Anoop Kurungadam <anoop@earthianslive.com>
2025-07-13 18:02:41 +0530

    feat(wip): cornerstone3D image viewer integration

---
+++ viewer/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from "vue"
+import App from "./App.vue"
+import "./index.css"
+
+// import { createUI } from "frappe-ui"
+
+const app = createApp(App)
+// app.use(createUI())
+app.mount("#app")
