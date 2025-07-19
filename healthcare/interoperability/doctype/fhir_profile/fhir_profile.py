fd191a456ab53b3698b56651c7c205176692ae0c
Anoop Kurungadam <anoop@earthianslive.com>
2025-07-19 15:32:51 +0530

    feat: new FHIRPackageImporter for structure definition import related changes to fhir version and fhir profile

--- healthcare/interoperability/doctype/fhir_profile/fhir_profile.py
+++ healthcare/interoperability/doctype/fhir_profile/fhir_profile.py
@@ -6,21 +6,33 @@ from frappe import _
 from frappe.model.document import Document
 from frappe.utils.background_jobs import enqueue
 
+from healthcare.interoperability.fhir_engine.fhir_package_importer import FHIRPackageImporter
+
 
 class FHIRProfile(Document):
 	@frappe.whitelist()
 	def import_structure_definitions(self):
-
 		if self.schema_file.endswith(".tgz") or self.schema_file.endswith(".tar.gz"):
 			enqueue(
-				"healthcare.interoperability.utils.fhir_utils.import_structure_definitions_from_package",
+				"healthcare.interoperability.doctype.fhir_profile.fhir_profile.import_fhir_package_job",
 				queue="long",
-				timeout=900,
+				job_name=f"Import FHIR Package {self.schema_file}",
+				package_tarball=self.schema_file,
 				version_name=self.fhir_version,
 				profile_name=self.name,
-				package_tarball=self.schema_file,
 			)
 		else:
 			frappe.throw(
-				_("Only .tgz / .tar.gz FHIR packages can be imported. " "Please upload a valid archive file.")
+				_(
+					"Only .tgz / .tar.gz FHIR packages can be imported for now. Please upload a valid archive file."
+				)
 			)
+
+
+def import_fhir_package_job(package_tarball, version_name, profile_name=None):
+	importer = FHIRPackageImporter(
+		package_tarball=package_tarball,
+		version_name=version_name,
+		profile_name=profile_name,
+	)
+	importer.import_package()
--- healthcare/interoperability/doctype/fhir_version/fhir_version.py
+++ healthcare/interoperability/doctype/fhir_version/fhir_version.py
@@ -6,20 +6,31 @@ from frappe import _
 from frappe.model.document import Document
 from frappe.utils.background_jobs import enqueue
 
+from healthcare.interoperability.fhir_engine.fhir_package_importer import FHIRPackageImporter
+
 
 class FHIRVersion(Document):
 	@frappe.whitelist()
 	def import_structure_definitions(self):
-
 		if self.schema_file.endswith(".tgz") or self.schema_file.endswith(".tar.gz"):
 			enqueue(
-				"healthcare.interoperability.utils.fhir_utils.import_structure_definitions_from_package",
+				"healthcare.interoperability.doctype.fhir_version.fhir_version.import_fhir_package_job",
 				queue="long",
-				timeout=900,
-				version_name=self.name,
+				job_name=f"Import FHIR Package {self.schema_file}",
 				package_tarball=self.schema_file,
+				version_name=self.name,
 			)
 		else:
 			frappe.throw(
-				_("Only .tgz / .tar.gz FHIR packages can be imported. " "Please upload a valid archive file.")
+				_(
+					"Only .tgz / .tar.gz FHIR packages can be imported for now. Please upload a valid archive file."
+				)
 			)
+
+
+def import_fhir_package_job(package_tarball, version_name):
+	importer = FHIRPackageImporter(
+		package_tarball=package_tarball,
+		version_name=version_name,
+	)
+	importer.import_package()
