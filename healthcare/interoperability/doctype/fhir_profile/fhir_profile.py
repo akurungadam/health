320645569247884af3e20144b74bac922f9e7400
Anoop Kurungadam <anoop@earthianslive.com>
2025-12-25 13:47:24 +0530

    feat: fhir version, fhir profile, fhir structure definition doctypes fhir package importer to import structure defs

---
+++ healthcare/interoperability/doctype/fhir_profile/fhir_profile.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2025, earthians Health Informatics Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from frappe.model.document import Document
+from frappe.utils.background_jobs import enqueue
+
+from healthcare.interoperability.fhir_engine.fhir_package_importer import FHIRPackageImporter
+
+
+class FHIRProfile(Document):
+	@frappe.whitelist()
+	def import_structure_definitions(self):
+		if self.schema_file.endswith(".tgz") or self.schema_file.endswith(".tar.gz"):
+			enqueue(
+				"healthcare.interoperability.doctype.fhir_profile.fhir_profile.import_fhir_package_job",
+				queue="long",
+				job_name=f"Import FHIR Package {self.schema_file}",
+				package_tarball=self.schema_file,
+				version_name=self.fhir_version,
+				profile_name=self.name,
+			)
+		else:
+			frappe.throw(
+				_(
+					"Only .tgz / .tar.gz FHIR packages can be imported for now. Please upload a valid archive file."
+				)
+			)
+
+
+def import_fhir_package_job(package_tarball, version_name, profile_name=None):
+	importer = FHIRPackageImporter(
+		package_tarball=package_tarball,
+		version_name=version_name,
+		profile_name=profile_name,
+	)
+	importer.import_package()
