55e667c6d78c3c566ccbff13ffab9e6131e69497
Anoop Kurungadam <anoop@earthianslive.com>
2026-03-31 19:18:03 +0530

    refactor: mthods to make patient, paractioner, clinical procedure template etc.

--- healthcare/tests/utils.py
+++ healthcare/tests/utils.py
@@ -1,5 +1,6 @@
 import frappe
 
+# from frappe.tests.utils import load_test_records_for
 from erpnext.tests.utils import ERPNextTestSuite
 
 
@@ -10,13 +11,122 @@ class BootStrapTestData:
 	def make_master_data(self):
 		self.make_company()
 		self.make_medical_department()
+		self.make_practitioner()
+		self.make_patient()
+		self.make_service_items()
+		self.make_appointment_types()
+		self.make_clinical_procedure_template()
+
+	def make_clinical_procedure_template(self):
+		records = [
+			{
+				"doctype": "Clinical Procedure Template",
+				"template": "_Test Procedure - Knee Surgery and Rehab",
+				"item_code": "_Test Procedure - Knee Surgery and Rehab",
+				"item_group": "Services",
+				"description": "Knee Surgery and Rehab",
+				"is_billable": 1,
+				"rate": 50000,
+			},
+		]
+		self.make_records(["template", "item_code"], records)
+
+	def make_appointment_types(self):
+		records = [
+			{
+				"doctype": "Appointment Type",
+				"appointment_type": "_Test Appointment Type",
+				"allow_booking_for": "Practitioner",
+				"medical_department": "_Test Medical Department",
+				"default_duration": 20,
+			},
+			{
+				"doctype": "Appointment Type",
+				"appointment_type": "_Test Appointment Type for Department",
+				"allow_booking_for": "Department",
+				"medical_department": "_Test Medical Department",
+				"default_duration": 20,
+			},
+			{
+				"doctype": "Appointment Type",
+				"appointment_type": "_Test Appointment Type with Items",
+				"allow_booking_for": "Practitioner",
+				"medical_department": "_Test Medical Department",
+				"default_duration": 20,
+				"items": [
+					{
+						"dt": "Medical Department",
+						"dn": "_Test Medical Department",
+						"op_consulting_charge_item": "HLC-SI-001",
+						"op_consulting_charge": 200,
+					},
+				],
+			},
+		]
+		self.make_records(["appointment_type"], records)
+
+	def make_service_items(self):
+		records = [
+			{
+				"doctype": "Item",
+				"item_code": "HLC-SI-001",
+				"item_name": "OP Consulting Charges",
+				"item_group": "Services",
+				"is_stock_item": 0,
+				"stock_uom": "Nos",
+			},
+			{
+				"doctype": "Item",
+				"item_code": "HLC-SI-002",
+				"item_name": "IP Consulting Charges",
+				"item_group": "Services",
+				"is_stock_item": 0,
+				"stock_uom": "Nos",
+			},
+		]
+		self.make_records(["item_code"], records)
+
+	def make_patient(self):
+		records = [
+			{
+				"doctype": "Patient",
+				"first_name": "_Test Patient",
+				"sex": "Male",
+			},
+		]
+		self.make_records(["first_name"], records)
+
+	def make_practitioner(self):
+		records = [
+			{
+				"doctype": "Healthcare Practitioner",
+				"first_name": "_Test Healthcare Practitioner",
+				"gender": "Female",
+				"department": "_Test Medical Department",
+				"op_consulting_charge": 500,
+				"inpatient_visit_charge": 500,
+			},
+		]
+		self.make_records(["first_name"], records)
 
 	def make_medical_department(self):
 		records = [
 			{
 				"doctype": "Medical Department",
 				"department": "_Test Medical Department",
-			}
+			},
+			{
+				"doctype": "Medical Department",
+				"department": "_Test Medical Department 0",
+			},
+			{
+				"doctype": "Medical Department",
+				"department": "_Test Medical Department 1",
+			},
+			{
+				"doctype": "Medical Department",
+				"department": "_Test Medical Department 2",
+			},
 		]
 		self.make_records(["department"], records)
 
@@ -29,7 +139,7 @@ class BootStrapTestData:
 				"default_currency": "INR",
 				"doctype": "Company",
 				"chart_of_accounts": "Standard",
-			}
+			},
 		]
 		self.make_records(["company_name"], records)
 
@@ -45,7 +155,6 @@ class BootStrapTestData:
 		for x in records:
 			filters = get_filters(x)
 			if not frappe.db.exists(doctype, filters):
-				print("H:", x)
 				frappe.get_doc(x).insert()
 
 
