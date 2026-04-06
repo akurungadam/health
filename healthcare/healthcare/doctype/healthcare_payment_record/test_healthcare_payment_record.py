de6d0845326d68a4da936b936d0b492c7491e5bf
Anoop Kurungadam <anoop@earthianslive.com>
2026-04-06 09:59:23 +0530

    chore: remove refs to EXTRA_TEST_RECORD_DEPENDENCIES

--- healthcare/healthcare/doctype/healthcare_payment_record/test_healthcare_payment_record.py
+++ healthcare/healthcare/doctype/healthcare_payment_record/test_healthcare_payment_record.py
@@ -4,12 +4,6 @@
 # import frappe
 from healthcare.tests.utils import HealthcareTestSuite
 
-# On IntegrationTestCase, the doctype test records and all
-# link-field test record dependencies are recursively loaded
-# Use these module variables to add/remove to/from that list
-EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
-IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
-
 
 class IntegrationTestHealthcarePaymentRecord(HealthcareTestSuite):
 	"""
