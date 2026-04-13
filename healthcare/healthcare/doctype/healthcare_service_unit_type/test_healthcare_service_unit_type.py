9d708c9ad12b7c010a389605fd2deef509e58a6a
Anoop Kurungadam <anoop@earthianslive.com>
2026-04-13 14:33:58 +0530

    fix(tests): service unit type name

--- healthcare/healthcare/doctype/healthcare_service_unit_type/test_healthcare_service_unit_type.py
+++ healthcare/healthcare/doctype/healthcare_service_unit_type/test_healthcare_service_unit_type.py
@@ -9,7 +9,9 @@ from healthcare.tests.utils import HealthcareTestSuite
 
 class TestHealthcareServiceUnitType(HealthcareTestSuite):
 	def test_item_creation(self):
-		unit_type = frappe.get_doc("Healthcare Service Unit Type", {"service_unit_type": "Inpatient Rooms"})
+		unit_type = frappe.get_doc(
+			"Healthcare Service Unit Type", {"service_unit_type": "_Test Inpatient Rooms"}
+		)
 		self.assertTrue(frappe.db.exists("Item", unit_type.item))
 
 		# check item disabled
